fe0f3f419437ff8bec02da73e850d33f3ef3fe8a
Sean Betts <seanbetts@icloud.com>
2024-09-05 15:23:55 +0100

    Mobile hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
-// src/components/Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { House, User, FolderOpen, Article, EnvelopeSimple } from "@phosphor-icons/react";
+import { House, User, FolderOpen, Article, EnvelopeSimple, List } from "@phosphor-icons/react";
 import ToggleTheme from './ToggleTheme';
 import styles from './Header.module.css';
-import profileImage from '../assets/sean-betts-profile.png'; // Make sure this path is correct
-import packageJson from '../../package.json'; // Import package.json
+import profileImage from '../assets/sean-betts-profile.png';
+import packageJson from '../../package.json';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
@@ -17,12 +22,15 @@ const Header = () => {
           </Link>
           <span className={styles.versionNumber}>v{packageJson.version}</span>
         </div>
-        <ul className={styles.navList}>
-          <li className={styles.navItem}><Link to="/" className={styles.navLink}><House size={24} /> Home</Link></li>
-          <li className={styles.navItem}><Link to="/projects" className={styles.navLink}><FolderOpen size={24} /> Projects</Link></li>
-          <li className={styles.navItem}><Link to="/writing" className={styles.navLink}><Article size={24} /> Writing</Link></li>
-          <li className={styles.navItem}><Link to="/about" className={styles.navLink}><User size={24} /> About</Link></li>
-          <li className={styles.navItem}><Link to="/contact" className={styles.navLink}><EnvelopeSimple size={24} /> Contact</Link></li>
+        <button className={styles.hamburgerMenu} onClick={toggleMenu}>
+          <List size={24} />
+        </button>
+        <ul className={`${styles.navList} ${isMenuOpen ? styles.open : ''}`}>
+          <li className={styles.navItem}><Link to="/" className={styles.navLink} onClick={toggleMenu}><House size={24} /> Home</Link></li>
+          <li className={styles.navItem}><Link to="/projects" className={styles.navLink} onClick={toggleMenu}><FolderOpen size={24} /> Projects</Link></li>
+          <li className={styles.navItem}><Link to="/writing" className={styles.navLink} onClick={toggleMenu}><Article size={24} /> Writing</Link></li>
+          <li className={styles.navItem}><Link to="/about" className={styles.navLink} onClick={toggleMenu}><User size={24} /> About</Link></li>
+          <li className={styles.navItem}><Link to="/contact" className={styles.navLink} onClick={toggleMenu}><EnvelopeSimple size={24} /> Contact</Link></li>
         </ul>
         <div className={styles.themeToggle}>
           <ToggleTheme />
