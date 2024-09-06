978bf0612fa42eb747092e58dcaf711c5a514a22
seanbetts <seanbetts@icloud.com>
2024-09-06 23:01:18 +0100

    Created articlesData

---
+++ src/data/articlesData.js
@@ -0,0 +1,60 @@
+const articlesData = [
+    {
+      title: "Scared and Excited",
+      url: "https://www.infosum.com/podcast/sean-betts-omg-scared-and-excited"
+    },
+    {
+      title: "Beyond Chatbots: A Blueprint for LLMs",
+      url: "https://www.the-blueprint.ai/p/beyond-chatbots-a-blueprint-for-llms"
+    },
+    {
+      title: "AI For The Rest Of Us",
+      url: "https://www.the-blueprint.ai/p/ai-for-the-rest-of-us"
+    },
+    {
+      title: "The Search for (im)perfection",
+      url: "https://newdigitalage.co/agencies/sean-betts-the-search-for-imperfection/"
+    },
+    {
+      title: "O is for Omni, but A is for Agent",
+      url: "https://www.the-blueprint.ai/p/o-is-for-omni-but-a-is-for-agent"
+    },
+    {
+      title: "The Marketing Singularity is Nearer Than You Think",
+      url: "https://newdigitalage.co/agencies/sean-betts-the-marketing-singularity-is-nearer-than-you-think/"
+    },
+    {
+      title: "Leadership Lessons",
+      url: "https://www.the-blueprint.ai/p/leadership-lessons"
+    },
+    {
+      title: "Tech Pail Kids",
+      url: "https://www.the-blueprint.ai/p/tech-pail-kids"
+    },
+    {
+      title: "The Power of GPTs",
+      url: "https://www.the-blueprint.ai/p/the-power-of-gpts"
+    },
+    {
+      title: "OpenAI DevDay Special",
+      url: "https://www.the-blueprint.ai/p/openai-devday-special"
+    },
+    {
+      title: "AI Safety Summit: what marketers need to know",
+      url: "https://www.thedrum.com/opinion/2023/11/07/ai-safety-summit-what-marketers-need-know"
+    },
+    {
+      title: "Is Generative AI a Feature or a Platform?",
+      url: "https://www.the-blueprint.ai/p/is-generative-ai-a-feature-or-a-platform"
+    },
+    {
+      title: "The Balancing Act",
+      url: "https://www.the-blueprint.ai/p/the-balancing-act"
+    },
+    {
+      title: "Navigating the role of generative AI in digital marketing",
+      url: "https://newdigitalage.co/agencies/sean-betts-the-generative-ai-hype-cycle/"
+    }
+  ];
+  
+  export default articlesData;
--- src/pages/Writing.js
+++ src/pages/Writing.js
@@ -3,71 +3,17 @@ import { Newspaper } from "@phosphor-icons/react";
 import styles from './Writing.module.css';
 import ArticlePreview from '../components/ArticlePreview';
 import fetchUrlMetadata from '../utils/fetchUrlMetadata';
-
-const initialArticles = [
-  {
-    title: "Beyond Chatbots: A Blueprint for LLMs",
-    url: "https://www.the-blueprint.ai/p/beyond-chatbots-a-blueprint-for-llms"
-  },
-  {
-    title: "AI For The Rest Of Us",
-    url: "https://www.the-blueprint.ai/p/ai-for-the-rest-of-us"
-  },
-  {
-    title: "The Search for (im)perfection",
-    url: "https://newdigitalage.co/agencies/sean-betts-the-search-for-imperfection/"
-  },
-  {
-    title: "O is for Omni, but A is for Agent",
-    url: "https://www.the-blueprint.ai/p/o-is-for-omni-but-a-is-for-agent"
-  },
-  {
-    title: "The Marketing Singularity is Nearer Than You Think",
-    url: "https://newdigitalage.co/agencies/sean-betts-the-marketing-singularity-is-nearer-than-you-think/"
-  },
-  {
-    title: "Leadership Lessons",
-    url: "https://www.the-blueprint.ai/p/leadership-lessons"
-  },
-  {
-    title: "Tech Pail Kids",
-    url: "https://www.the-blueprint.ai/p/tech-pail-kids"
-  },
-  {
-    title: "The Power of GPTs",
-    url: "https://www.the-blueprint.ai/p/the-power-of-gpts"
-  },
-  {
-    title: "OpenAI DevDay Special",
-    url: "https://www.the-blueprint.ai/p/openai-devday-special"
-  },
-  {
-    title: "AI Safety Summit: what marketers need to know",
-    url: "https://www.thedrum.com/opinion/2023/11/07/ai-safety-summit-what-marketers-need-know"
-  },
-  {
-    title: "Is Generative AI a Feature or a Platform?",
-    url: "https://www.the-blueprint.ai/p/is-generative-ai-a-feature-or-a-platform"
-  },
-  {
-    title: "The Balancing Act",
-    url: "https://www.the-blueprint.ai/p/the-balancing-act"
-  },
-  {
-    title: "Navigating the role of generative AI in digital marketing",
-    url: "https://newdigitalage.co/agencies/sean-betts-the-generative-ai-hype-cycle/"
-  }
-];
+import articlesData from '../data/articlesData';
 
 const Writing = () => {
-  const [articles, setArticles] = useState(initialArticles.map(article => ({ ...article, loading: true })));
+  const [articles, setArticles] = useState(articlesData.map(article => ({ ...article, loading: true })));
 
   const fetchMetadata = useCallback(async () => {
     const cachedMetadata = JSON.parse(localStorage.getItem('articleMetadata')) || {};
     const currentTime = new Date().getTime();
     const cacheExpiration = 24 * 60 * 60 * 1000; // 24 hours
 
-    const updatedArticles = await Promise.all(initialArticles.map(async (article, index) => {
+    const updatedArticles = await Promise.all(articlesData.map(async (article, index) => {
       if (article.url !== "#") {
         const cachedData = cachedMetadata[article.url];
         if (cachedData && (currentTime - cachedData.timestamp) < cacheExpiration) {
